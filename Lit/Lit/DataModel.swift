5023c7fd22a1588f18cd5c9b7ebd5c2f63c94088
Simon Moushabeck <smoushab@searles224-15.bowdoin.edu>
2016-05-07 18:03:29 -0400

    AHAAHAHAH

---
+++ Lit/Lit/DataModel.swift
@@ -0,0 +1,14 @@
+//
+//  MapModel.swift
+//  Lit
+//
+//  Created by Simon Moushabeck on 5/7/16.
+//  Copyright © 2016 Jack Chris and Simon. All rights reserved.
+//
+
+import Foundation
+
+class Map {
+    static var eventsList : [Event] = []
+    static var venuesList : [Venue] = []
+}
